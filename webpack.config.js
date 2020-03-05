269234fe5a0ebbf3b44016838ea1262a0b0a0102
Manoel Freitas <manoel.josefneto@outlook.com>
2020-03-04 23:48:35 -0300

    fix production build

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,34 @@
 const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 const mode = process.env.NODE_ENV
+
+const plugins = []
+
+if (mode === 'production') {
+  plugins.push(new CopyPlugin([
+    { from: 'app.css', to: 'app.css' },
+  ]))
+  plugins.push(new HtmlWebpackPlugin({
+    filename: path.resolve(__dirname, 'dist/index.html'),
+    template: 'index.html',
+    inject: true,
+    // minify: {
+    //   removeComments: true,
+    //   collapseWhitespace: true,
+    //   removeAttributeQuotes: true
+    // },
+    chunksSortMode: 'dependency'
+  }))
+}
+
+if (mode === 'development') {
+  plugins.push(new HtmlWebpackPlugin({
+    filename: 'index.html',
+    template: 'index.html',
+    inject: true
+  }))
+}
 
 module.exports = {
   mode,
@@ -7,7 +36,7 @@ module.exports = {
   output: {
     filename: 'bundle.js',
     path: path.resolve(__dirname, 'dist'),
-    publicPath: 'dist'
+    publicPath: mode === 'production' ? '' : '/'
   },
   module: {
     rules: [
@@ -20,5 +49,6 @@ module.exports = {
   },
   resolve: {
     extensions: ['.ts', '.js']
-  }
+  },
+  plugins
 };
